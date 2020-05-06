7184a059101b57998f76c6e873e8422468990014
mihahauke <kempka.michal@gmail.com>
2020-05-06 23:40:58 +0200

    Removed redundancy from qlearning agents

--- lab6/qlearningAgents.py
+++ lab6/qlearningAgents.py
@@ -19,7 +19,7 @@ from misio.pacman.util import CustomCounter, lookup
 import random, math
 
 
-class QLearningAgent(ReinforcementAgent):
+class PacmanQAgent(ReinforcementAgent):
     """
       Q-Learning Agent
 
@@ -40,8 +40,22 @@ class QLearningAgent(ReinforcementAgent):
           which returns legal actions for a state
     """
 
-    def __init__(self, **args):
+    def __init__(self,
+                 epsilon=0.05,
+                 gamma=0.8,
+                 alpha=0.2,
+                 numTraining=0,
+                 extractor=IdentityExtractor(),
+                 **args):
         "You can initialize Q-values here..."
+
+        args['epsilon'] = epsilon
+        args['gamma'] = gamma
+        args['alpha'] = alpha
+        args['numTraining'] = numTraining
+        self.featExtractor = extractor
+        self.index = 0  # This is always Pacman
+        self.weights = CustomCounter()
         ReinforcementAgent.__init__(self, **args)
 
         "*** YOUR CODE HERE ***"
@@ -91,6 +105,9 @@ class QLearningAgent(ReinforcementAgent):
         "*** YOUR CODE HERE ***"
         raise NotImplementedError()
 
+        action = None
+        self.doAction(state, action)
+
         return action
 
     def update(self, state, action, nextState, reward):
@@ -111,66 +128,8 @@ class QLearningAgent(ReinforcementAgent):
     def getValue(self, state):
         return self.computeValueFromQValues(state)
 
-
-class PacmanQAgent(QLearningAgent):
-    "Exactly the same as QLearningAgent, but with different default parameters"
-
-    def __init__(self, epsilon=0.05, gamma=0.8, alpha=0.2, numTraining=0, **args):
-        """
-        alpha    - learning rate
-        epsilon  - exploration rate
-        gamma    - discount factor
-        numTraining - number of training episodes, i.e. no learning after these many episodes
-        """
-        args['epsilon'] = epsilon
-        args['gamma'] = gamma
-        args['alpha'] = alpha
-        args['numTraining'] = numTraining
-        self.index = 0  # This is always Pacman
-        QLearningAgent.__init__(self, **args)
-
-    def getAction(self, state):
-        """
-        Simply calls the getAction method of QLearningAgent and then
-        informs parent of action for Pacman.  Do not change or remove this
-        method.
-        """
-        action = QLearningAgent.getAction(self, state)
-        self.doAction(state, action)
-        return action
-
-
-class ApproximateQAgent(PacmanQAgent):
-    """
-       ApproximateQLearningAgent
-
-       You should only have to overwrite getQValue
-       and update.  All other QLearningAgent functions
-       should work as is.
-    """
-
-    def __init__(self, extractor=IdentityExtractor, **args):
-        self.featExtractor = IdentityExtractor()
-        PacmanQAgent.__init__(self, **args)
-        self.weights = CustomCounter()
-
     def getWeights(self):
         return self.weights
-
-    def getQValue(self, state, action):
-        """
-          Should return Q(state,action) = w * featureVector
-          where * is the dotProduct operator
-        """
-        "*** YOUR CODE HERE ***"
-        raise NotImplementedError()
-
-    def update(self, state, action, nextState, reward):
-        """
-           Should update your weights based on transition
-        """
-        "*** YOUR CODE HERE ***"
-        raise NotImplementedError()
 
     def final(self, state):
         "Called at the end of each game."
@@ -182,3 +141,7 @@ class ApproximateQAgent(PacmanQAgent):
             # you might want to print your weights here for debugging
             "*** YOUR CODE HERE ***"
             pass
+
+
+
+
