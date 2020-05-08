57340a6c4200450c871e193303a2777184f82648
mihahauke <kempka.michal@gmail.com>
2020-05-09 00:34:24 +0200

    Corrected sample solution for mdp2

--- lab5/sample_solution.py
+++ lab5/sample_solution.py
@@ -1,5 +1,18 @@
-from misio.lost_wumpus.agents import RandomAgent
-from misio.optilio import run_agent
+#!/usr/bin/python3
+
+
+import numpy as np
+import sys
 
 if __name__ == "__main__":
-    run_agent(RandomAgent, "ignore")
+    instances_num = int(input())
+    for _ in range(instances_num):
+        max_cars = int(input())
+        gamma = float(input())
+        _ = [float(x) for x in input().split()]
+        _ = float(input())
+        _ = float(input())
+
+        to_move = int(input())
+        random_policy = np.random.randint(-to_move, to_move, (max_cars + 1, max_cars + 1), dtype=np.int)
+        np.savetxt(sys.stdout, random_policy, fmt='%2d')
