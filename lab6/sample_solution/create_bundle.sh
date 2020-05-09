41cbb23466e7e75a6f9b9ddc66f646e3109b9e21
Michał Kempka <kempka.michal@gmail.com>
2020-05-09 17:02:57 +0200

    Update create_bundle.sh

--- lab6/sample_solution/create_bundle.sh
+++ lab6/sample_solution/create_bundle.sh
@@ -3,4 +3,7 @@
 main_file="my_random_solution.py"
 archive="${main_file}".tgz
 
+chmod +x ${main_file}
+chmod +x ${train.sh}
+
 tar -cvzf ${archive} ${main_file} weights.txt dummy_file.py instruction.md train.sh
