70cb26be35fcda0cafd1f7602ad2aa57cb893f32
Tomasz Kiljańczyk <39602955+Gunock@users.noreply.github.com>
2021-08-16 17:00:57 +0200

    Update util.py
    Fixed generate_deterministic_seeds function

--- misio/util.py
+++ misio/util.py
@@ -3,5 +3,5 @@ import numpy as _np
 
 def generate_deterministic_seeds(seed, shape):
     r = _np.random.RandomState(seed=seed)
-    seeds = r.randint(0, _np.iinfo(_np.uint32).max, shape)
+    seeds = r.randint(0, _np.iinfo(_np.int32).max, shape)
     return seeds
